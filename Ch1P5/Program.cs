bc38c251f279e742e8eacca9f3cb340cbc38cf6c
jonathonroach <roach.jonathon4@gmail.com>
2022-08-28 17:03:04 -0500

    adding other files

---
+++ Ch1P5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ch1P5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Ch1P6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ch1P6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Another simple program");
+        }
+    }
+}
