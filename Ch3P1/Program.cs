43ddf3b8b69445fc3a350909a8f5442bf29f3615
Roach Jonathon <KMU6422@hca.corpad.net>
2022-11-01 09:10:16 -0500

    adding ch3 exercises

---
+++ Ch3P1/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Write an expression that checks whether an integer is odd or even.*/
+
+using System;
+
+public class Ch3P1
+{
+    static void Main()
+    {
+        //int a = 1;
+        int a = 2;
+
+        if (a % 2 == 0)
+        {
+            Console.WriteLine("Even");
+        }
+        else
+        {
+            Console.WriteLine("Odd");
+        }
+    }
+
+}
