1611240e123e43da14c24094ae0a727cdff61a4b
Roach Jonathon <KMU6422@hca.corpad.net>
2022-11-01 11:59:43 -0500

    more ch3 solutions

---
+++ Ch3P3/Ch3P3.cs
@@ -0,0 +1,14 @@
+﻿/*Write an expression that checks for a given integer if its third digit (right to left) is 7.*/
+using System;
+
+public class Ch3P3
+{
+    static void Main()
+    {
+        //int a = 751702;
+        int a = 10750215;
+        bool check = (a / 100) % 10 == 7;
+
+        Console.WriteLine(check ? "Third Digit is 7": "Third Digit is not 7");
+    }
+}
