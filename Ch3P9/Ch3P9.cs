b915bb3969aff2c66b3a83c970febc8c4c4224a8
Roach Jonathon <KMU6422@hca.corpad.net>
2022-11-02 14:31:36 -0500

    adding ch3 problem 9

---
+++ Ch3P9/Ch3P9.cs
@@ -0,0 +1,20 @@
+﻿/*Write an expression that checks for given point {x, y} 
+ * if it is within the circle K[{0, 0}, R=5] and out of the 
+ * rectangle [{-1, 1}, {5, 5}]. Clarification: for the rectangle 
+ * the lower left and the upper right corners are given.*/
+
+public class Ch3P9
+{
+    static void Main()
+    {
+        Console.Write("Enter x: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter y: ");
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        bool inCircle = Math.Pow(x, 2) + Math.Pow(y, 2) <= 25;
+        bool outOfRectangle = (x < -1 || x > 5) || (y < 1 || y > 5);
+
+        Console.WriteLine(inCircle && outOfRectangle ? "Yes" : "No");
+    }
+}
