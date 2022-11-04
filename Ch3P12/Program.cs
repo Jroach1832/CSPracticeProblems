b9519ed8d404e3bfa73c51bcd90a210332999011
Roach Jonathon <KMU6422@hca.corpad.net>
2022-11-04 09:07:13 -0500

    add ch3 problem 13

---
+++ Ch3P12/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Write a Boolean expression that checks if the bit on position p in the integer v has the value 1. Example v=5, p=1 -> false.*/
+using System;
+
+public class Ch3P12
+{
+    static void Main()
+    {
+        int v = 5;
+        int p = 1;
+        int i = 1;
+        int mask = i << p;
+
+        Console.WriteLine((mask & v) != 0);
+
+    }
+}
---
+++ Ch3P13/Ch3P13.cs
@@ -0,0 +1,32 @@
+﻿
+/*We are given the number n, the value v (v = 0 or 1) 
+ * and the position p. write a sequence of operations 
+ * that changes the value of n, so the bit on the position 
+ * p has the value of v. Example: n=35, p=5, v=0 -> n=3.
+ * Another example: n=35, p=2, v=1 -> n=39.*/
+
+using System;
+
+public class Ch3P13
+{
+    static void Main()
+    {
+        int n = 35; //number
+        int p = 2; //position
+        int v = 1; //value
+        int i = 1;
+
+        int nNew = v == 0 ? ~(i << p) & n : (i << p) | n;
+
+        Console.WriteLine("Input: ");
+        Console.WriteLine($"Binary Representation: {Convert.ToString(n, 2)}");
+        Console.WriteLine($"Decimal Representation: {n}");
+
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Output:");  
+        Console.WriteLine($"Binary Representation: {Convert.ToString(nNew, 2)}");
+        Console.WriteLine($"Decimal Representation: {nNew}");
+
+    }
+}
