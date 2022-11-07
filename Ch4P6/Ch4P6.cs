29b62ce704c22887b2274b33ba19208961848158
Roach Jonathon <KMU6422@hca.corpad.net>
2022-11-07 08:19:42 -0600

    adding ch4 problem 6

---
+++ Ch4P6/Ch4P6.cs
@@ -0,0 +1,19 @@
+﻿/*Write a program that reads two numbers from the console 
+ * and prints the greater of them. Solve the problem without 
+ * using conditional statements.*/
+
+using System;
+
+public class Ch4P6
+{
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("The greater of the two entries is {0}", Math.Max(a, b));
+    }
+}
