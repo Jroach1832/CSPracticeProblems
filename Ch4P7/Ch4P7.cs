ff572b35877260138e9664116fab202ec5432d53
Roach Jonathon <KMU6422@hca.corpad.net>
2022-11-07 09:50:35 -0600

    adding CH4 problem 7

---
+++ Ch4P7/Ch4P7.cs
@@ -0,0 +1,36 @@
+﻿/*Write a program that reads five integer 
+ * numbers and prints their sum. If an invalid 
+ * number is entered the program should prompt 
+ * the user to enter another number.*/
+
+//Neat way of doing input validation found here:
+//https://learn.microsoft.com/en-us/dotnet/api/system.int32.tryparse?view=net-6.0
+
+using System;
+
+public class Ch4P7
+{
+    static void Main()
+    {
+        long total = 0;
+
+        for (int i = 0; i < 5; i++)
+        {
+
+            bool valid = int.TryParse(Console.ReadLine(), out int number);
+
+            if (valid)
+            {
+                total += number;
+            }
+            else
+            {
+                i--;
+            }
+
+            
+        }
+
+        Console.WriteLine(total);
+    }
+}
